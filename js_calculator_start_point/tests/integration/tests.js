b99272ca9bcdc39a4a6d67aedfd6c1e3f16de76c
Eden <edenstevens101@gmail.com>
2017-08-12 13:13:39 +0100

    Int Test - Chaining operations together passed

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -59,5 +59,16 @@ describe('calculator functionality', function() {
        expect(running_total.getAttribute('value')).to.eventually.equal('2.5')
      })
 
+  it('should update display chaining operations', function(){
+       running_total = element(by.css('#running_total'))
+       element(by.css('#number9')).click();
+       element(by.css('#operator_divide')).click();
+       element(by.css('#number3')).click();
+       element(by.css('#operator_subtract')).click();
+       element(by.css('#number2')).click();
+       element(by.css('#operator_equals')).click();
+       expect(running_total.getAttribute('value')).to.eventually.equal('1.5')
+     })
+
 
   });
