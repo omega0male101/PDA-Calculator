d085fc0a46b4c3871ec6f0d7aae796148c5b0890
Eden <edenstevens101@gmail.com>
2017-08-12 13:08:51 +0100

    Integration Test 2 passed.

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -32,6 +32,14 @@ describe('calculator functionality', function() {
      expect(running_total.getAttribute('value')).to.eventually.equal('17')
    })
 
+  it('should update display for subtraction', function(){
+       running_total = element(by.css('#running_total'))
+       element(by.css('#number9')).click();
+       element(by.css('#operator_subtract')).click();
+       element(by.css('#number1')).click();
+       element(by.css('#operator_equals')).click();
+       expect(running_total.getAttribute('value')).to.eventually.equal('8')
+    })
 
 
   });
