79b7b2b54d5c2b08322513920d34fffbe4ede18e
Eden <edenstevens101@gmail.com>
2017-08-12 13:10:27 +0100

    Integration Test 4 passed.

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -50,5 +50,14 @@ describe('calculator functionality', function() {
        expect(running_total.getAttribute('value')).to.eventually.equal('15')
     })
 
+  it('should update display for division', function(){
+       running_total = element(by.css('#running_total'))
+       element(by.css('#number6')).click();
+       element(by.css('#operator_divide')).click();
+       element(by.css('#number3')).click();
+       element(by.css('#operator_equals')).click();
+       expect(running_total.getAttribute('value')).to.eventually.equal('4.5')
+     })
+
 
   });
